5ccbbfacb2933fcf366697e755fe246c57593dee
nmartin <nate.martin27@gmail.com>
2024-12-03 12:21:10 -0500

    inline styling

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const cards = {
+display: 'flex',
+margin: '20px 20px 20px 20px',
+padding: '15px 50px',
+gap: '20px',
 }
 
-export default App
+const frontend = {
+  color: 'white',
+  fontsize: '50pt',
+  backgroundColor: 'red',
+  borderRadius: '5px',
+  padding: '15px 50px',
+};
+
+const backend = {
+  color: 'black',
+  fontsize: '50pt',
+  backgroundColor: 'yellow',
+  borderRadius: '5px',
+  padding: '15px 50px',
+
+};
+
+const comfortStyle = {
+  color: 'white',
+  fontsize: '50pt',
+  backgroundColor: 'teal',
+  borderRadius: '5px',
+  padding: '15px 50px',
+
+  };
+
+const App = () => {
+  const skills = { text: "Learn something new", frontEnd: true };
+  const mySkills = [
+    {
+      name: "JavaScript",
+      comfort: 8,
+      frontEnd: true,
+      backend: true,
+    },
+    {
+      name: "CSS",
+      comfort: 9,
+      frontEnd: true,
+      backend: false,
+    },
+    {
+      name: "HTML",
+      comfort: 9,
+      frontEnd: true,
+      backend: false,
+    },
+    {
+      name: "React/JSX",
+      comfort: 5,
+      frontEnd: false,
+      backend: false,
+    },
+  ];
+  return (
+    < >
+    
+      <h1>My Skills:</h1>
+      <div style={cards}>
+      {/* <p>{skills.text}</p> */}
+      <ul style={frontend}>
+      <h3>Frontend:</h3>
+
+        {mySkills.map((skills, index) => (
+          <p key={index}>
+            {skills.frontEnd ? ` FrontEnd Completed - ${skills.name}` : `Working on it - ${skills.name}`}
+          </p>
+        ))}
+      </ul>
+      <ul style={backend}>
+      <h3>Backend:</h3>
+
+        {mySkills.map((skills, index) =>
+        <p key={index}>
+         {skills.backend ? ` Backend Completed - ${skills.name}` : `Working on it - ${skills.name}`}
+        
+     </p>
+    )}
+      </ul>
+      <div>
+        <ul style={comfortStyle}>
+           <h3>Comfort Level:</h3>
+           {mySkills.map((skills) => (
+           
+        <p>{skills.name} - {skills.comfort}</p> 
+      ))}
+      </ul>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
